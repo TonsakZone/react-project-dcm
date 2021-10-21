c8ce098cf41090e0a95c7130b0c70634dca607f2
Supphakorn Innuphat <91209210+TonsakZone@users.noreply.github.com>
2021-10-21 20:57:59 +0700

    V1.0

--- src/content/Home.js
+++ src/content/Home.js
@@ -1,11 +1,31 @@
 import React from 'react'
+import '../App.css';
 
 function Home() {
     return (
-        <div className="custom home">
-            <h1>Welcome to project</h1>
-            <p></p>
-            <h3>React web</h3>
+        <div className="home wrapper">
+            <h2>Welcome to react project</h2>
+            <br/>
+            <h2>DCM viewer and CSV viewer</h2>
+            <a href="https://www.kaggle.com/c/siim-covid19-detection/data" target="_blank" rel="noreferrer">DCM image and CSV file from Kaggle</a>
+            <br/>
+            <a href="https://www.youtube.com/watch?v=qx7pSLjLNQA" target="_blank" rel="noreferrer">Extra: Animate background using CSS</a>
+            <div className="box">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+            <div className="triangle">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+            <div className="circle">
+                <div></div>
+                <div></div>
+            </div>
         </div>
     )
 }
