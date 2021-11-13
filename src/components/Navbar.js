f86a4d8de2edc162490a88238198e53b36e366f7
Supphakorn Innuphat <91209210+TonsakZone@users.noreply.github.com>
2021-11-13 21:17:14 +0700

    Patch date 13/11/21 (Rework Navbar.js)

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,24 @@
 import React from 'react'
 import Navitems from './Navitems.js'
+import { SocialIcon } from 'react-social-icons';
+import { RiReactjsLine } from 'react-icons/ri';
+import './Navigator.css';
 
 function Navbar() {
     return (
         <nav>
             <ul>
+                <div className="App-logo">
+                    <RiReactjsLine />
+                </div>
                 <Navitems item="Home" tolink="/"></Navitems>
-                <Navitems item="100GB" tolink="/image"></Navitems>
-                <Navitems item="CSV viewer" tolink="/csv"></Navitems>
+                <Navitems item="DCM viewer" tolink="/image"></Navitems>
+                <Navitems item="train_study_level.csv" tolink="/trainstudy"></Navitems>
+                <Navitems item="sample_submission.csv" tolink="/samplesub"></Navitems>
+                <Navitems item="train_image_level.csv" tolink="/trainimage"></Navitems>
+                <SocialIcon url="https://www.facebook.com/supphakorn.innuphat.3/" target="_blank" />
+                <p></p>
+                <SocialIcon url="https://www.instagram.com/tonsak14.3/" target="_blank" />
             </ul>
         </nav>
     );
